74251f3f293880958371f6ff555137b8980bd775
Tristan <thrawst88@gmail.com>
2015-01-08 22:01:25 -0700

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+gulp.task('less', function () {
+  gulp.src('./css/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('./css/'))
+    .pipe(reload({ stream:true }));
+});
+
+gulp.task('reload', function(){
+	browserSync.reload();
+});
+
+gulp.task('serve', function() {
+  browserSync({
+    server: {
+      baseDir: '.'
+    }
+  });
+
+  gulp.watch('css/*.less', ['less', 'reload']);
+  gulp.watch(['*.html', 'js/**/*.js'], {cwd:'.'}, reload);
+});
+
---
+++ js/main.js
@@ -0,0 +1,25 @@
+var app = angular.module('DrakeEquation', []);
+
+app.controller('equationController', ['$scope', function($scope){
+  $scope.R = 7;
+  $scope.fp = 0.4;
+  $scope.n = 2;
+  $scope.fl = 0.5;
+  $scope.fi = 0.1;
+  $scope.fc = 0.1;
+  $scope.L = 400;
+
+  $scope.N = function(n){
+    n = Math.round(n);
+    if (n == 0){
+      return "Life should be impossible, we are a fluke.";
+    }
+    else if (n == 1){
+      return "We are alone in our galaxy.";
+    }
+    else {
+      return n + " contactable civilizations in our galaxy.";
+    }
+  }
+  
+}]);
